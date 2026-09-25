637d4067129eaaa5fdc0dff3ebc62855d605d459
Gustavo Junqueira Colas <gustavocolas@gmail.com>
2026-09-25 13:12:33 -0300

    corrigindo alguns códigos

--- IPA labirinto/define_algoritmos.py
+++ IPA labirinto/define_algoritmos.py
@@ -1,4 +1,4 @@
-import time
+# DEFINE ALGORITMOS 
 
 def segue_parede(robo, max_passos=3000, visualizar=False, atraso=0.03):
     while not robo.chegou_no_objetivo() and robo.passos < max_passos:
@@ -18,7 +18,7 @@ def segue_parede(robo, max_passos=3000, visualizar=False, atraso=0.03):
 def executar_algoritmo_customizado(
         robo, lista_dos_comandos, 
         max_passos=3000,
-        # tentou_parar = False,
+        tentou_parar_func=None,
         funcao_de_desenho=None):
 
     acoes = [
@@ -27,9 +27,12 @@ def executar_algoritmo_customizado(
     ]
     
     while (not robo.venceu() and
-            robo.passos < max_passos 
-            # and not tentou_parar
-            ):
+            robo.passos < max_passos):
+        
+        # Checa se o usuário pediu para parar durante a execução
+        if tentou_parar_func and tentou_parar_func():
+            break
+            
         acao_executada = False
         i = 0
         
@@ -38,9 +41,8 @@ def executar_algoritmo_customizado(
             
             if comando == 'se':
                 i += 1
-                condicao_tokens = [] # abre a aba do 'se' pra avaliar a expressão formada
+                condicao_tokens = []
                 
-                # coleta todos os sensores e operadores até encontrar uma ação (ou o final do script)
                 while (i < len(lista_dos_comandos) and 
                         lista_dos_comandos[i] not in acoes and
                         lista_dos_comandos[i] not in ['se', 'senao']):
@@ -48,10 +50,8 @@ def executar_algoritmo_customizado(
                     condicao_tokens.append(lista_dos_comandos[i])
                     i += 1
                     
-                # a ação a ser executada será o token imediatamente após a condição
                 acao_alvo = lista_dos_comandos[i] if (i < len(lista_dos_comandos) and lista_dos_comandos[i] in acoes) else None
                 
-                # traduz a expressão 'natural' pra 'linguagem de programação'
                 expressao = []
                 for tok in condicao_tokens:
                     if tok == 'e': 
@@ -67,30 +67,25 @@ def executar_algoritmo_customizado(
                 condicao_verdadeira = False
                 if expressao:
                     try:
-                        # EVAL é a chave que permite avaliar as sentenças!!!
                         condicao_verdadeira = eval(" ".join(expressao), {"robo": robo})
                     except Exception:
-                        # se a lógica não faz sentido - tipo montar parênteses vazios,
-                        # ele vai simplesmente considerar falso
                         condicao_verdadeira = False
                         pass 
                         
                 if condicao_verdadeira and acao_alvo:
                     getattr(robo, acao_alvo)()
                     acao_executada = True
-                    break # recomeça o loop do algoritmo para o próximo passo
+                    break
                     
-                # se a expressao final foi falsa, pula a ação para não executá-la
                 if acao_alvo:
                     i += 1 
                     
-            elif comando == 'senao': # é um elif
+            elif comando == 'senao':
                 i += 1
                 if i < len(lista_dos_comandos) and lista_dos_comandos[i] in acoes:
                     getattr(robo, lista_dos_comandos[i])()
                     acao_executada = True
                     break
-                
         
             elif comando in acoes:
                 getattr(robo, comando)()
@@ -98,13 +93,12 @@ def executar_algoritmo_customizado(
                 break
                 
             else:
-                i += 1 # CORREÇÃO: Ignora blocos quebrados/soltos e continua a leitura
+                i += 1
                 
-        # atualização da tela
+        # Atualização da tela com o passo do robô
         if acao_executada and funcao_de_desenho:
             funcao_de_desenho()
             
-        # impede o jogo de travar se o algoritmo gerar um loop que nunca é executado
         if not acao_executada:
             break
             
